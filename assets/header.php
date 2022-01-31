d7f662232cb28c7484dc84470793098d8af20563
Chiraag Sanjeev Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2022-01-31 12:31:35 +0530

    Update header.php

--- assets/header.php
+++ assets/header.php
@@ -7,15 +7,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-L2FML28316"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-L2FML28316');
-</script>
   <!-- BOOTSTRSP CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- BOOTSTRSP CSS -->
